9256f88b149204cb2bb3816312d513a2ef615bac
Akshay Bhatia <akshaybhatia10@gmail.com>
2016-09-22 17:45:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,19 @@
-console.log('Loaded!');
+<!doctype html>
+<html>
+    <head>
+        <link href="/ui/style.css" rel="stylesheet" />
+        <meta name="viewport" content="width-device-width, initial-scale=1"/>
+
+    </head>
+    <body>
+        <div class="center">
+            <img src="madi.png" class="img-medium"/>
+        </div>
+        <br>
+        <div class="center text-big bold">
+            Hi! I am your webapp.
+        </div>
+        <script type="text/javascript" src="/ui/main.js">
+        </script>
+    </body>
+</html>
