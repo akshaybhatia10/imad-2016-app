c16805ba11fb0d146e98788533a1691fb4b0eb2a
Akshay Bhatia <akshaybhatia10@gmail.com>
2016-10-29 12:38:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(morgan('combined'));
 
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/first',function(req,res){
